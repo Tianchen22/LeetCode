8ff8bbee9a057bb383658d4071a7dfdf54786a76
Tianchen22 <56239703+Tianchen22@users.noreply.github.com>
2019-11-25 18:49:12 +1100

    Create 188.py

---
+++ Unsolved/188.py
@@ -0,0 +1,8 @@
+"""
+188. Best Time to Buy and Sell Stock IV
+
+https://leetcode.com/problems/best-time-to-buy-and-sell-stock-iv/
+
+The first thought is dp question
+dp[n][k] (n numbers with k transactions), but hasn't found the state transition equation
+"""
