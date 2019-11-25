59fe4b1c51fe689cda4e821f01655adea7444f83
Tianchen22 <56239703+Tianchen22@users.noreply.github.com>
2019-11-25 18:37:29 +1100

    Create 1192.py

---
+++ Unsolved/1192.py
@@ -0,0 +1,8 @@
+"""
+1192. Critical Connections in a Network
+
+https://leetcode.com/problems/critical-connections-in-a-network/
+
+The question is to Find the bridge in an un-directed graph
+seems to use Tarjan algorithm
+"""
